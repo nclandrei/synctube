41e3df794fc555494cc3f7b0c74c045342c55e24
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-06-25 18:15:37 +0200

    commented youtube til it's stable

--- vendor/app/controller/youtube.go
+++ vendor/app/controller/youtube.go
@@ -1,37 +1,37 @@
-package controller
-
-import (
-	"net/http"
-	"app/shared/youtube"
-	"fmt"
-)
-
-// Get user's playlists after getting access
-func YouTubeGET(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func printPlaylistsListResults(response *youtube.PlaylistListResponse) {
-	for _, item := range response.Items {
-		fmt.Println(item.Id, ": ", item.Snippet.Title)
-	}
-}
-
-func playlistsListMine(service *youtube.Service, part string, mine bool, maxResults int64, onBehalfOfContentOwner string, onBehalfOfContentOwnerChannel string) {
-	call := service.Playlists.List(part)
-	if mine {
-		call = call.Mine(mine)
-	}
-	if maxResults != 0 {
-		call = call.MaxResults(maxResults)
-	}
-	if onBehalfOfContentOwner != "" {
-		call = call.OnBehalfOfContentOwner(onBehalfOfContentOwner)
-	}
-	if onBehalfOfContentOwnerChannel != "" {
-		call = call.OnBehalfOfContentOwnerChannel(onBehalfOfContentOwnerChannel)
-	}
-	response, err := call.Do()
-	handleError(err, "")
-	printPlaylistsListResults(response)
-}
+//package controller
+//
+//import (
+//	"net/http"
+//	"app/shared/youtube"
+//	"fmt"
+//)
+//
+//// Get user's playlists after getting access
+//func YouTubeGET(w http.ResponseWriter, r *http.Request) {
+//
+//}
+//
+//func printPlaylistsListResults(response *youtube.PlaylistListResponse) {
+//	for _, item := range response.Items {
+//		fmt.Println(item.Id, ": ", item.Snippet.Title)
+//	}
+//}
+//
+//func playlistsListMine(service *youtube.Service, part string, mine bool, maxResults int64, onBehalfOfContentOwner string, onBehalfOfContentOwnerChannel string) {
+//	call := service.Playlists.List(part)
+//	if mine {
+//		call = call.Mine(mine)
+//	}
+//	if maxResults != 0 {
+//		call = call.MaxResults(maxResults)
+//	}
+//	if onBehalfOfContentOwner != "" {
+//		call = call.OnBehalfOfContentOwner(onBehalfOfContentOwner)
+//	}
+//	if onBehalfOfContentOwnerChannel != "" {
+//		call = call.OnBehalfOfContentOwnerChannel(onBehalfOfContentOwnerChannel)
+//	}
+//	response, err := call.Do()
+//	handleError(err, "")
+//	printPlaylistsListResults(response)
+//}
