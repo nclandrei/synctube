fcaad7f87ce4db80b77e1a3a2d2d63fc6be95ce5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-06-25 19:47:37 +0200

    added code for accessing youtube

--- vendor/app/controller/youtube.go
+++ vendor/app/controller/youtube.go
@@ -1,37 +1,134 @@
-//package controller
-//
-//import (
-//	"net/http"
-//	"app/shared/youtube"
-//	"fmt"
-//)
-//
-//// Get user's playlists after getting access
-//func YouTubeGET(w http.ResponseWriter, r *http.Request) {
-//
-//}
-//
-//func printPlaylistsListResults(response *youtube.PlaylistListResponse) {
-//	for _, item := range response.Items {
-//		fmt.Println(item.Id, ": ", item.Snippet.Title)
-//	}
-//}
-//
-//func playlistsListMine(service *youtube.Service, part string, mine bool, maxResults int64, onBehalfOfContentOwner string, onBehalfOfContentOwnerChannel string) {
-//	call := service.Playlists.List(part)
-//	if mine {
-//		call = call.Mine(mine)
-//	}
-//	if maxResults != 0 {
-//		call = call.MaxResults(maxResults)
-//	}
-//	if onBehalfOfContentOwner != "" {
-//		call = call.OnBehalfOfContentOwner(onBehalfOfContentOwner)
-//	}
-//	if onBehalfOfContentOwnerChannel != "" {
-//		call = call.OnBehalfOfContentOwnerChannel(onBehalfOfContentOwnerChannel)
-//	}
-//	response, err := call.Do()
-//	handleError(err, "")
-//	printPlaylistsListResults(response)
-//}
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/youtube/v3"
+)
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("youtube-go-quickstart.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func handleError(err error, message string) {
+	if message == "" {
+		message = "Error making API call"
+	}
+	if err != nil {
+		log.Fatalf(message + ": %v", err.Error())
+	}
+}
+
+func channelsListByUsername(service *youtube.Service, part string, forUsername string) {
+	call := service.Channels.List(part)
+	call = call.ForUsername(forUsername)
+	response, err := call.Do()
+	handleError(err, "")
+	fmt.Println(fmt.Sprintf("This channel's ID is %s. Its title is '%s', " +
+		"and it has %d views.",
+		response.Items[0].Id,
+		response.Items[0].Snippet.Title,
+		response.Items[0].Statistics.ViewCount))
+}
+
+
+func main() {
+	ctx := context.Background()
+
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/youtube-go-quickstart.json
+	config, err := google.ConfigFromJSON(b, youtube.YoutubeReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
+	service, err := youtube.New(client)
+
+	handleError(err, "Error creating YouTube client")
+
+	channelsListByUsername(service, "snippet,contentDetails,statistics", "GoogleDevelopers")
+}
