8b9ed6589c1f55513dbbf6368b56729e360d7617
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-02 04:27:41 +0200

    added function to retrieve refresh token from mongodb

--- shared/database/database.go
+++ shared/database/database.go
@@ -1,24 +1,17 @@
 package database
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"time"
 
-	"github.com/boltdb/bolt"
 	_ "github.com/go-sql-driver/mysql" // MySQL driver
-	"github.com/jmoiron/sqlx"
 	"gopkg.in/mgo.v2"
 )
 
+//noinspection GoNameStartsWithPackageName
 var (
-	// BoltDB wrapper
-	BoltDB *bolt.DB
 	// Mongo wrapper
 	Mongo *mgo.Session
-	// SQL wrapper
-	SQL *sqlx.DB
 	// Database info
 	databases Info
 )
@@ -27,7 +20,6 @@ var (
 type Type string
 
 const (
-
 	// TypeMongoDB is MongoDB
 	TypeMongoDB Type = "MongoDB"
 )
@@ -36,10 +28,6 @@ const (
 type Info struct {
 	// Database type
 	Type Type
-	// MySQL info if used
-	MySQL MySQLInfo
-	// Bolt info if used
-	Bolt BoltInfo
 	// MongoDB info if used
 	MongoDB MongoDBInfo
 }
@@ -50,20 +38,6 @@ type MongoDBInfo struct {
 	Database string
 }
 
-// DSN returns the Data Source Name
-func DSN(ci MySQLInfo) string {
-	// Example: root:@tcp(localhost:3306)/test
-	return ci.Username +
-		":" +
-		ci.Password +
-		"@tcp(" +
-		ci.Hostname +
-		":" +
-		fmt.Sprintf("%d", ci.Port) +
-		")/" +
-		ci.Name + ci.Parameter
-}
-
 // Connect to the database
 func Connect(d Info) {
 	var err error
@@ -71,104 +45,19 @@ func Connect(d Info) {
 	// Store the config
 	databases = d
 
-	switch d.Type {
-	case TypeMySQL:
-		// Connect to MySQL
-		if SQL, err = sqlx.Connect("mysql", DSN(d.MySQL)); err != nil {
-			log.Println("SQL Driver Error", err)
-		}
-
-		// Check if is alive
-		if err = SQL.Ping(); err != nil {
-			log.Println("Database Error", err)
-		}
-	case TypeBolt:
-		// Connect to Bolt
-		if BoltDB, err = bolt.Open(d.Bolt.Path, 0600, nil); err != nil {
-			log.Println("Bolt Driver Error", err)
-		}
-	case TypeMongoDB:
-		// Connect to MongoDB
-		if Mongo, err = mgo.DialWithTimeout(d.MongoDB.URL, 5*time.Second); err != nil {
-			log.Println("MongoDB Driver Error", err)
-			return
-		}
-
-		// Prevents these errors: read tcp 127.0.0.1:27017: i/o timeout
-		Mongo.SetSocketTimeout(1 * time.Second)
-
-		// Check if is alive
-		if err = Mongo.Ping(); err != nil {
-			log.Println("Database Error", err)
-		}
-	default:
-		log.Println("No registered database in config")
+	// Connect to MongoDB
+	if Mongo, err = mgo.DialWithTimeout(d.MongoDB.URL, 5*time.Second); err != nil {
+		log.Println("MongoDB Driver Error", err)
+		return
 	}
-}
 
-// Update makes a modification to Bolt
-func Update(bucketName string, key string, dataStruct interface{}) error {
-	err := BoltDB.Update(func(tx *bolt.Tx) error {
-		// Create the bucket
-		bucket, e := tx.CreateBucketIfNotExists([]byte(bucketName))
-		if e != nil {
-			return e
-		}
+	// Prevents these errors: read tcp 127.0.0.1:27017: i/o timeout
+	Mongo.SetSocketTimeout(1 * time.Second)
 
-		// Encode the record
-		encodedRecord, e := json.Marshal(dataStruct)
-		if e != nil {
-			return e
-		}
-
-		// Store the record
-		if e = bucket.Put([]byte(key), encodedRecord); e != nil {
-			return e
-		}
-		return nil
-	})
-	return err
-}
-
-// View retrieves a record in Bolt
-func View(bucketName string, key string, dataStruct interface{}) error {
-	err := BoltDB.View(func(tx *bolt.Tx) error {
-		// Get the bucket
-		b := tx.Bucket([]byte(bucketName))
-		if b == nil {
-			return bolt.ErrBucketNotFound
-		}
-
-		// Retrieve the record
-		v := b.Get([]byte(key))
-		if len(v) < 1 {
-			return bolt.ErrInvalid
-		}
-
-		// Decode the record
-		e := json.Unmarshal(v, &dataStruct)
-		if e != nil {
-			return e
-		}
-
-		return nil
-	})
-
-	return err
-}
-
-// Delete removes a record from Bolt
-func Delete(bucketName string, key string) error {
-	err := BoltDB.Update(func(tx *bolt.Tx) error {
-		// Get the bucket
-		b := tx.Bucket([]byte(bucketName))
-		if b == nil {
-			return bolt.ErrBucketNotFound
-		}
-
-		return b.Delete([]byte(key))
-	})
-	return err
+	// Check if is alive
+	if err = Mongo.Ping(); err != nil {
+		log.Println("Database Error", err)
+	}
 }
 
 // CheckConnection returns true if MongoDB is available
