4b6da913fee76516254ebf39a0c9a77d74d1bd38
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-02 07:22:53 +0200

    fully functional now, can get likes using the token stored in the DB

--- controller/youtube-sync.go
+++ controller/youtube-sync.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"context"
 	"github.com/nclandrei/YTSync/shared/session"
+	//"log"
+	"log"
 )
 
 const (
@@ -43,55 +45,49 @@ func YouTubePOST(w http.ResponseWriter, r *http.Request) {
 	// retrieve the playlist ID for uploads to the authenticated user's
 	// channel.
 	call := service.Channels.List("contentDetails").Mine(true)
-	//secondCall := service.Playlists.List("contentDetails").Mine(true)
 
-	//response, err := call.Do()
-	//if err != nil {
-	//	// The channels.list method call returned an error.
-	//	log.Fatalf("Error making API call to list channels: %v", err.Error())
-	//}
-	//
-	//responseSecond, _ := secondCall.Do()
-	//for _, secondItem := range responseSecond.Items {
-	//	fmt.Println(secondItem.Snippet.Title)
-	//}
+	response, err := call.Do()
+	if err != nil {
+		// The channels.list method call returned an error.
+		log.Fatalf("Error making API call to list channels: %v", err.Error())
+	}
 
-	//for _, channel := range response.Items {
-	//	playlistId := channel.ContentDetails.RelatedPlaylists.Likes
-	//	// Print the playlist ID for the list of uploaded videos.
-	//	fmt.Printf("Videos in list %s\r\n", playlistId)
-	//
-	//	nextPageToken := ""
-	//	for {
-	//		// Call the playlistItems.list method to retrieve the
-	//		// list of uploaded videos. Each request retrieves 50
-	//		// videos until all videos have been retrieved.
-	//		playlistCall := service.PlaylistItems.List("snippet").
-	//			PlaylistId(playlistId).
-	//			MaxResults(50).
-	//			PageToken(nextPageToken)
-	//
-	//		playlistResponse, err := playlistCall.Do()
-	//
-	//		if err != nil {
-	//			// The playlistItems.list method call returned an error.
-	//			log.Fatalf("Error fetching playlist items: %v", err.Error())
-	//		}
-	//
-	//		for _, playlistItem := range playlistResponse.Items {
-	//			title := playlistItem.Snippet.Title
-	//			videoId := playlistItem.Snippet.ResourceId.VideoId
-	//			fmt.Printf("%v, (%v)\r\n", title, videoId)
-	//		}
-	//
-	//		// Set the token to retrieve the next page of results
-	//		// or exit the loop if all results have been retrieved.
-	//		nextPageToken = playlistResponse.NextPageToken
-	//		if nextPageToken == "" {
-	//			break
-	//		}
-	//		fmt.Println()
-	//	}
-	//}
+	for _, channel := range response.Items {
+		playlistId := channel.ContentDetails.RelatedPlaylists.Likes
+		// Print the playlist ID for the list of uploaded videos.
+		fmt.Printf("Videos in list %s\r\n", playlistId)
+
+		nextPageToken := ""
+		for {
+			// Call the playlistItems.list method to retrieve the
+			// list of uploaded videos. Each request retrieves 50
+			// videos until all videos have been retrieved.
+			playlistCall := service.PlaylistItems.List("snippet").
+				PlaylistId(playlistId).
+				MaxResults(50).
+				PageToken(nextPageToken)
+
+			playlistResponse, err := playlistCall.Do()
+
+			if err != nil {
+				// The playlistItems.list method call returned an error.
+				log.Fatalf("Error fetching playlist items: %v", err.Error())
+			}
+
+			for _, playlistItem := range playlistResponse.Items {
+				title := playlistItem.Snippet.Title
+				videoId := playlistItem.Snippet.ResourceId.VideoId
+				fmt.Printf("%v, (%v)\r\n", title, videoId)
+			}
+
+			// Set the token to retrieve the next page of results
+			// or exit the loop if all results have been retrieved.
+			nextPageToken = playlistResponse.NextPageToken
+			if nextPageToken == "" {
+				break
+			}
+			fmt.Println()
+		}
+	}
 	http.Redirect(w, r, "/", http.StatusFound)
 }
