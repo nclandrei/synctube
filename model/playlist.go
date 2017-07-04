082c46e3c57a8f8b16c537e9a3e6c5615708f0c5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-04 23:11:38 +0200

    started implementing the model for the playlist

--- model/playlist.go
+++ model/playlist.go
@@ -1,1 +1,171 @@
 package model
+
+import (
+	"time"
+
+	"github.com/nclandrei/YTSync/shared/database"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// *****************************************************************************
+// Playlist
+// *****************************************************************************
+
+// Playlist table contains the information for each playlist per user
+type Playlist struct {
+	ObjectID  bson.ObjectId `bson:"_id"`
+	ID        uint32        `db:"id" bson:"id,omitempty"`
+	Content   string        `db:"content" bson:"content"`
+	UserID    bson.ObjectId `bson:"user_id"`
+	UID       uint32        `db:"user_id" bson:"userid,omitempty"`
+	CreatedAt time.Time     `db:"created_at" bson:"created_at"`
+	UpdatedAt time.Time     `db:"updated_at" bson:"updated_at"`
+	Deleted   uint8         `db:"deleted" bson:"deleted"`
+}
+
+// PlaylistID returns the note id
+func (u *Playlist) PlaylistID() string {
+	r := ""
+	r = u.ObjectID.Hex()
+	return r
+}
+
+// PlaylistByID gets note by ID
+func PlaylistByID(userID string, playlistID string) (Playlist, error) {
+	var err error
+
+	result := Playlist{}
+
+	if database.CheckConnection() {
+		// Create a copy of mongo
+		session := database.Mongo.Copy()
+		defer session.Close()
+		c := session.DB(database.ReadConfig().MongoDB.Database).C("playlist")
+
+		// Validate the object id
+		if bson.IsObjectIdHex(playlistID) {
+			err = c.FindId(bson.ObjectIdHex(noteID)).One(&result)
+			if result.UserID != bson.ObjectIdHex(userID) {
+				result = Playlist{}
+				err = ErrUnauthorized
+			}
+		} else {
+			err = ErrNoResult
+		}
+	} else {
+		err = ErrUnavailable
+	}
+
+	return result, standardizeError(err)
+}
+
+// PlaylistByUserID gets all playlists for a user
+func PlaylistByUserID(userID string) ([]Playlist, error) {
+	var err error
+
+	var result []Playlist
+
+	if database.CheckConnection() {
+		// Create a copy of mongo
+		session := database.Mongo.Copy()
+		defer session.Close()
+		c := session.DB(database.ReadConfig().MongoDB.Database).C("playlist")
+
+		// Validate the object id
+		if bson.IsObjectIdHex(userID) {
+			err = c.Find(bson.M{"user_id": bson.ObjectIdHex(userID)}).All(&result)
+		} else {
+			err = ErrNoResult
+		}
+	} else {
+		err = ErrUnavailable
+	}
+
+	return result, standardizeError(err)
+}
+
+// NoteCreate creates a note
+func PlaylistCreate(content string, userID string) error {
+	var err error
+
+	now := time.Now()
+
+	if database.CheckConnection() {
+		// Create a copy of mongo
+		session := database.Mongo.Copy()
+		defer session.Close()
+		c := session.DB(database.ReadConfig().MongoDB.Database).C("note")
+
+		note := &Note{
+			ObjectID:  bson.NewObjectId(),
+			Content:   content,
+			UserID:    bson.ObjectIdHex(userID),
+			CreatedAt: now,
+			UpdatedAt: now,
+			Deleted:   0,
+		}
+		err = c.Insert(note)
+	} else {
+		err = ErrUnavailable
+	}
+
+	return standardizeError(err)
+}
+
+// NoteUpdate updates a note
+func NoteUpdate(content string, userID string, noteID string) error {
+	var err error
+
+	now := time.Now()
+
+	if database.CheckConnection() {
+		// Create a copy of mongo
+		session := database.Mongo.Copy()
+		defer session.Close()
+		c := session.DB(database.ReadConfig().MongoDB.Database).C("note")
+		var note Note
+		note, err = NoteByID(userID, noteID)
+		if err == nil {
+			// Confirm the owner is attempting to modify the note
+			if note.UserID.Hex() == userID {
+				note.UpdatedAt = now
+				note.Content = content
+				err = c.UpdateId(bson.ObjectIdHex(noteID), &note)
+			} else {
+				err = ErrUnauthorized
+			}
+		}
+	} else {
+		err = ErrUnavailable
+	}
+
+	return standardizeError(err)
+}
+
+// NoteDelete deletes a note
+func NoteDelete(userID string, noteID string) error {
+	var err error
+
+	if database.CheckConnection() {
+		// Create a copy of mongo
+		session := database.Mongo.Copy()
+		defer session.Close()
+		c := session.DB(database.ReadConfig().MongoDB.Database).C("note")
+
+		var note Note
+		note, err = NoteByID(userID, noteID)
+		if err == nil {
+			// Confirm the owner is attempting to modify the note
+			if note.UserID.Hex() == userID {
+				err = c.RemoveId(bson.ObjectIdHex(noteID))
+			} else {
+				err = ErrUnauthorized
+			}
+		}
+	} else {
+		err = ErrUnavailable
+	}
+
+	return standardizeError(err)
+}
