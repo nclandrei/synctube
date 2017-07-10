716c22dfed912ebc6066b794a420542c1df3f55c
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-11 01:06:46 +0200

    added skeleton for the file manager

---
+++ shared/file_manager/file_manager.go
@@ -0,0 +1,11 @@
+package file_manager
+
+import "github.com/nclandrei/YTSync/model"
+
+func CreateFolders(videos []model.Video) error {
+
+}
+
+func CleanUp() error {
+	// TODO: add code to remove zip/mp3 files after being downloaded by the user
+}
