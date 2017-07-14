1b3f24c95520c56ab42d7e110a377c4053ef2314
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-14 14:42:45 +0200

    added explanatory comments to authorization functions for youtube

--- shared/youtube/auth/auth.go
+++ shared/youtube/auth/auth.go
@@ -1,12 +1,13 @@
 package auth
 
 import (
-	"golang.org/x/oauth2"
-	"log"
 	"context"
-	"net/http"
-	"github.com/nclandrei/YTSync/model"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/nclandrei/YTSync/model"
+	"golang.org/x/oauth2"
 )
 
 /**
@@ -28,15 +29,15 @@ var (
 	ytConfig oauth2.Config
 )
 
-// function that sets up the YT instance
+// Configure - used for setting up the YT instance
 func Configure(config oauth2.Config) {
 	ytConfig = config
 }
 
-func GetClient (ctx context.Context, code string, userId string) *http.Client {
+func GetClient(ctx context.Context, code string, userId string) *http.Client {
 	tok, err := getTokenFromDb(userId)
 	fmt.Println(tok)
-	if err != nil || !tok.Valid(){
+	if err != nil || !tok.Valid() {
 		tok, _ = getTokenFromWeb(code)
 		updateTokenInDb(userId, tok)
 	}
@@ -49,7 +50,7 @@ func GetAuthorizationURL() string {
 	return url
 }
 
-// GetTokenFromWeb - given an authorization code it returns the token from a page
+// given an authorization code it returns the token from a page
 func getTokenFromWeb(code string) (*oauth2.Token, error) {
 	tok, err := ytConfig.Exchange(context.Background(), code)
 	if err != nil {
@@ -58,8 +59,7 @@ func getTokenFromWeb(code string) (*oauth2.Token, error) {
 	return tok, err
 }
 
-// tokenFromFile retrieves a Token from a given file path.
-// It returns the retrieved Token and any read error encountered.
+// returns the token for a specific user from the database
 func getTokenFromDb(userId string) (*oauth2.Token, error) {
 	currentUser, err := model.UserByToken(userId)
 	if err != nil {
@@ -68,6 +68,7 @@ func getTokenFromDb(userId string) (*oauth2.Token, error) {
 	return &currentUser.Token, err
 }
 
+// given a new token it replaces the already existing one in DB
 func updateTokenInDb(userID string, token *oauth2.Token) {
 	err := model.UpdateUserToken(userID, *token)
 	if err != nil {
@@ -80,7 +81,6 @@ func handleError(err error, message string) {
 		message = "Error making API call"
 	}
 	if err != nil {
-		log.Fatalf(message + ": %v", err.Error())
+		log.Fatalf(message+": %v", err.Error())
 	}
 }
-
