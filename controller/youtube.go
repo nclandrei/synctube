3502ef8fafa49e592d10fd6475d4328594f9a346
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-18 10:20:01 +0200

    fetching in YT controller from fetcher functions now

--- controller/youtube.go
+++ controller/youtube.go
@@ -10,6 +10,7 @@ import (
 	"github.com/nclandrei/synctube/model"
 	"github.com/nclandrei/synctube/shared/session"
 	"github.com/nclandrei/synctube/shared/youtube/auth"
+	"github.com/nclandrei/synctube/shared/youtube/fetcher"
 	"github.com/nclandrei/synctube/shared/youtube/file_manager"
 	"google.golang.org/api/youtube/v3"
 )
@@ -50,102 +51,32 @@ func YouTubePOST(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Could not retrieve client - %v", err.Error())
 	}
 
-	// First call - will retrieve all items in Likes playlist;
-	// needs special call as it is a different kind of playlist
-	call := service.Channels.List("contentDetails").Mine(true)
+	likedVideos := fetcher.FetchLikes(userID, service)
+	userPlaylistVideos := fetcher.FetchUserPlaylistVideos(userID, service)
 
-	response, err := call.Do()
-	if err != nil {
-		// The channels.list method call returned an error.
-		log.Fatalf("Error making API call to list channels: %v", err.Error())
+	var toAddVideos []model.Video
+
+	if !isPlaylistNew {
+		storedVideos, err := model.VideosByPlaylistID(playlistId)
+		if err != nil {
+			log.Fatalf("Error when retrieving all videos in playlist: %v", err.Error())
+		}
+		toAddVideos = diffPlaylistVideos(videos, storedVideos)
+		toDeleteVideos := diffPlaylistVideos(storedVideos, videos)
+		for _, item := range toDeleteVideos {
+			model.VideoDelete(item.ID, item.PlaylistID)
+		}
+	} else {
+		toAddVideos = videos
 	}
 
-	for _, channel := range response.Items {
-		var isPlaylistNew bool
-		playlistId := channel.ContentDetails.RelatedPlaylists.Likes
-
-		// Print the playlist ID for the list of uploaded videos.
-		fmt.Printf("Videos in list %s\r\n", playlistId)
-
-		_, err := model.PlaylistByID(playlistId, userID)
-
-		if err == model.ErrNoResult {
-			isPlaylistNew = true
-			err := model.PlaylistCreate(playlistId, "Likes", userID)
-			if err != nil {
-				log.Fatalf("Error creating playlist: %v", err.Error())
-			}
-			log.Printf("Added Likes playlist for user ID %v", userID)
-		} else if err != model.ErrNoResult && err != nil {
-			log.Fatalf("Error fetching Likes playlist from the database: %v", err.Error())
+	for _, item := range toAddVideos {
+		err := model.VideoCreate(item.ID, item.Title, item.PlaylistID)
+		if err != nil {
+			log.Fatalf("Error adding the video to the database: %v", err.Error())
 		}
-
-		nextPageToken := ""
-		var videos []model.Video
-
-		for {
-			playlistCall := service.PlaylistItems.List("snippet").
-				PlaylistId(playlistId).
-				MaxResults(50).
-				PageToken(nextPageToken)
-
-			playlistResponse, err := playlistCall.Do()
-
-			if err != nil {
-				log.Fatalf("Error fetching playlist items: %v", err.Error())
-			}
-
-			for _, playlistItem := range playlistResponse.Items {
-				title := playlistItem.Snippet.Title
-				videoId := playlistItem.Snippet.ResourceId.VideoId
-
-				video := model.Video{
-					ID:         videoId,
-					Title:      title,
-					PlaylistID: playlistId,
-				}
-
-				videos = append(videos, video)
-				log.Printf("YouTube video - (title: %v, ID: %v)", title, videoId)
-			}
-
-			// Set the token to retrieve the next page of results
-			// or exit the loop if all results have been retrieved.
-			nextPageToken = playlistResponse.NextPageToken
-
-			if nextPageToken == "" {
-				break
-			}
-			fmt.Println()
-		}
-
-		var toAddVideos []model.Video
-
-		if !isPlaylistNew {
-			storedVideos, err := model.VideosByPlaylistID(playlistId)
-			if err != nil {
-				log.Fatalf("Error when retrieving all videos in playlist: %v", err.Error())
-			}
-			toAddVideos = diffPlaylistVideos(videos, storedVideos)
-			toDeleteVideos := diffPlaylistVideos(storedVideos, videos)
-			for _, item := range toDeleteVideos {
-				model.VideoDelete(item.ID, item.PlaylistID)
-			}
-		} else {
-			toAddVideos = videos
-		}
-
-		for _, item := range toAddVideos {
-			err := model.VideoCreate(item.ID, item.Title, item.PlaylistID)
-			if err != nil {
-				log.Fatalf("Error adding the video to the database: %v", err.Error())
-			}
-			log.Printf("adding item with title '%v' to mongo", item.Title)
-		}
-
+		log.Printf("adding item with title '%v' to mongo", item.Title)
 	}
-
-	// user created playlists - will retrieve all items in user created playlists
 
 	// Finally, before redirecting to homepage, save the timestamp of the this sync
 	err = model.UserUpdateLastSync(userID, time.Now())
