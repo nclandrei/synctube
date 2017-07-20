fe1a8233c95d20f458378560fbf21563fc5caed1
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-20 17:30:40 +0200

    commented appropriately the whole flow in the YouTube controller

--- controller/youtube.go
+++ controller/youtube.go
@@ -21,11 +21,14 @@ const (
 	oauthStateString string = "random"
 )
 
+// YouTubeGET - handles getting the authorization code from Google
 func YouTubeGET(w http.ResponseWriter, r *http.Request) {
 	authURL := auth.GetAuthorizationURL()
 	http.Redirect(w, r, authURL, http.StatusTemporaryRedirect)
 }
 
+// YouTubePOST - handles all the processing of the user's YouTube account and then returns
+// the zip containing all synced files
 func YouTubePOST(w http.ResponseWriter, r *http.Request) {
 	state := r.FormValue("state")
 	sess := session.Instance(r)
@@ -36,25 +39,32 @@ func YouTubePOST(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// get the Google auth code from the session values
 	code := r.FormValue("code")
 	userID := fmt.Sprintf("%s", sess.Values["id"])
 
+	// create a service that will perform the API calls to YouTube data
 	client := auth.GetClient(context.Background(), code, userID)
-
 	service, err := youtube.New(client)
 	if err != nil {
 		log.Fatalf("Could not retrieve client - %v", err.Error())
 	}
 
+	// fetch all videos from all channels for the currently logged in user
 	videosMap := fetcher.FetchVideos(userID, service)
 
+	// synchronize and return a playlist-videos_list map containing all videos that
+	// have been fetched and are not in the database
 	toDownloadVideosMap := synchronizer.Synchronize(videosMap)
 
+	// download all videos previously returned by the synchronizer
 	err = downloader.DownloadYouTubeVideos(toDownloadVideosMap)
 
+	// create temporary user and playlist folders, create zip, return it to user
+	// and, in the end, clean up everything
 	err = file_manager.ManageFiles(userID, toDownloadVideosMap)
 
-	// Finally, before redirecting to homepage, save the timestamp of the this sync
+	// finally, before redirecting to homepage, save the timestamp of the this sync
 	err = model.UserUpdateLastSync(userID, time.Now())
 	if err != nil {
 		log.Fatalf("Error updating last sync timestamp for user: %v", err.Error())
