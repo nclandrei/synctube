5c6c1b06a1e3d868e8d0754b9caa416e26e60ca5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-07-26 01:19:54 +0200

    added missing trailing backslash to download videos

--- shared/youtube/downloader/downloader.go
+++ shared/youtube/downloader/downloader.go
@@ -23,7 +23,7 @@ func DownloadYouTubeVideos(userID string, videosMap map[model.Playlist][]model.V
 		for _, video := range videos {
 			log.Printf("Downloading YouTube video for user %v - (title: %v, ID: %v)", userID, video.Title, video.ID)
 			fullURL := fmt.Sprintf("%v%v", youtubePrefix, video.ID)
-			output := fmt.Sprintf("--output '%v/%v/%v%%(title)s.%%(ext)s'", tmpFolderPath, userID, playlist.Title)
+			output := fmt.Sprintf("--output '%v/%v/%v/%%(title)s.%%(ext)s'", tmpFolderPath, userID, playlist.Title)
 			args := []string{extractAudio, audioFormat, output, fullURL}
 			command := youtubeDownloadCmd
 			for _, arg := range args {
