e599e45f2bfd69dd9357812009dc60efc9c40e6f
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-05-07 00:32:45 +0100

    Add base types for playlists and videos

--- playlist.go
+++ playlist.go
@@ -1,1 +1,20 @@
-package main
+package synctube
+
+import (
+	"hash"
+	"net/url"
+)
+
+// Playlist describes the information held by a YouTube playlist.
+type Playlist struct {
+	Name   string
+	Videos []Video
+	hash   hash.Hash
+}
+
+// Video describes the information held by a YouTube video.
+type Video struct {
+	Name   string
+	URL    url.URL
+	Length float64
+}
