589fb9073937295666ee10b78e312fe11cb10ce9
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2019-02-02 03:18:34 +0100

    Start working on a CLI command

--- main.go
+++ main.go
@@ -1,61 +1,38 @@
-package synctube
+package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/youtube/v3"
-	"io/ioutil"
+	"github.com/rylio/ytdl"
 	"log"
 	"os"
-	"os/user"
-	"time"
+	"strings"
 )
 
-const configFile = ".synctube"
-
-// Config defines the configuration needed to sync playlists and videos.
-type Config struct {
-	Playlists  []string  `json:"playlists"`
-	LastUpdate time.Time `json:"last_update"`
-}
-
 func main() {
-	currUser, err := user.Current()
+	vi, err := ytdl.GetVideoInfo("https://www.youtube.com/watch?v=AHNtfiovZhc")
 	if err != nil {
-		log.Fatalf("could not retrieve current user's home directory: %v\n", err)
-	}
-	f, err := os.Open(fmt.Sprintf("%s/%s", currUser.HomeDir, configFile))
-	if err != os.ErrNotExist {
-		log.Fatalf("could not open configuration file: %v\n", err)
+		log.Fatalf("could not get video info: %v\n", err)
 	}
 
-	fBody, err := ioutil.ReadAll(f)
+	var format ytdl.Format
+	for _, f := range vi.Formats {
+		if f.Extension != "mp4" {
+			continue
+		}
+		if f.AudioBitrate > format.AudioBitrate {
+			format = f
+		}
+	}
+
+	filename := strings.Replace(vi.Title, " ", "_", -1)
+	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatalf("could not read file body: %v\n", err)
+		log.Fatalf("could not create file: %v\n", err)
 	}
+	defer file.Close()
 
-	var c Config
-	err = json.Unmarshal(fBody, &c)
+
+	err = vi.Download(format, file)
 	if err != nil {
-		log.Fatalf("could not unmarshal config file: %v\n", err)
+		log.Fatalf("could not download file: %v\n", err)
 	}
-
-	ctx := context.Background()
-
-	b, err := ioutil.ReadFile("client_secret.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	// If modifying these scopes, delete your previously saved credentials
-	// at ~/.credentials/youtube-go-quickstart.json
-	config, err := google.ConfigFromJSON(b, youtube.YoutubeReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	service, err := NewYTService(ctx, config)
-	service.Playlists.List("test")
-
 }
